2997775e26cd58c8e28b2919ade7325b37d2332e
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-07-28 17:05:22 -0700

    implemented functionality to list the courses a user have already enrolled near the top of home page, added link to profile in home page if a user have logged in, and show username.

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../models/user.model.client';
+import {UserServiceClient} from '../services/user.service.client';
+import {CourseServiceClient} from '../services/course.service.client';
+import {Course} from '../models/coruse.model.client';
+import {SectionServiceClient} from '../services/section.service.client';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +13,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserServiceClient,
+              private courseService: CourseServiceClient,
+              private sectionService: SectionServiceClient,
+              private router: Router) { }
+
+  user = new User();
+  userId;
+  courses: Course[] = [];
+  enrolledCourses: Course[] = [];
+  sections = [];
 
   ngOnInit() {
+    this.userService
+      .profile()
+      .then(user => {
+        if (!user.err) {
+          this.user = user;
+          this.userId = user._id;
+          this.courseService.findAllCourses()
+            .then(courses => this.courses = courses)
+            .then(() => {
+              this.sectionService
+                .findSectionsForStudent(this.userId)
+                .then(sections => this.sections = sections)
+                .then(() => {
+                  for (let i = 0; i < this.sections.length; i++) {
+                    for (let j = 0; j < this.courses.length; j++) {
+                      if (this.sections[i].section.courseId === this.courses[j].id) {
+                        this.enrolledCourses.push(this.courses[j]);
+                      }
+                    }
+                  }
+                });
+            });
+        }
+      }).then(() => console.log(this.userId));
   }
 
 }
