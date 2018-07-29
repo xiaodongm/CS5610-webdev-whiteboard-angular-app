bb219f2c907339659a0624391810397175b4b228
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-07-28 18:25:17 -0700

    implemented functionality which a user can navigate to the course section from profile.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,6 +3,8 @@ import {User} from '../models/user.model.client';
 import {UserServiceClient} from '../services/user.service.client';
 import {Router} from '@angular/router';
 import {SectionServiceClient} from '../services/section.service.client';
+import {CourseServiceClient} from '../services/course.service.client';
+import {Course} from '../models/coruse.model.client';
 
 @Component({
   selector: 'app-profile',
@@ -13,12 +15,15 @@ export class ProfileComponent implements OnInit {
 
   constructor(private service: UserServiceClient,
               private sectionService: SectionServiceClient,
+              private courseService: CourseServiceClient,
               private router: Router) { }
 
   // user = {};
   user = new User();
   userId;
   sections = [];
+  courses: Course[] = [];
+  enrolledCourses: Course[] = [];
 
   update() {
     console.log(this.user);
@@ -34,10 +39,33 @@ export class ProfileComponent implements OnInit {
 
   }
 
+  findEnrolledCourses() {
+    this.courseService.findAllCourses()
+      .then(courses => this.courses = courses)
+      .then(() => {
+        this.sectionService
+          .findSectionsForStudent(this.userId)
+          .then(sections => this.sections = sections)
+          .then(() => {
+            for (let i = 0; i < this.sections.length; i++) {
+              for (let j = 0; j < this.courses.length; j++) {
+                if (this.sections[i].section.courseId === this.courses[j].id) {
+                  this.enrolledCourses = new Array();
+                  this.enrolledCourses.push(this.courses[j]);
+                  return this.enrolledCourses;
+                }
+              }
+            }
+          });
+      });
+  }
+
   unEnroll(userId, sectionId) {
     this.sectionService.unEnrollSection(userId, sectionId)
       .then(() => this.sectionService.findSectionsForStudent(userId))
-      .then(sections => this.sections = sections);
+      .then(sections => this.sections = sections)
+      .then(() => this.findEnrolledCourses())
+      .then(enrolledCourses => this.enrolledCourses);
   }
 
   ngOnInit() {
@@ -49,12 +77,51 @@ export class ProfileComponent implements OnInit {
         } else {
           this.user = user;
           this.userId = user._id;
+          this.courseService.findAllCourses()
+            .then(courses => this.courses = courses)
+            .then(() => {
+              this.sectionService
+                .findSectionsForStudent(this.userId)
+                .then(sections => this.sections = sections)
+                .then(() => {
+                  for (let i = 0; i < this.sections.length; i++) {
+                    for (let j = 0; j < this.courses.length; j++) {
+                      if (this.sections[i].section.courseId === this.courses[j].id) {
+                        this.enrolledCourses.push(this.courses[j]);
+                      }
+                    }
+                  }
+                });
+            })
+            .then(() => {
+            console.log(this.sections);
+            console.log(this.courses);
+            console.log(this.userId);
+          });
         }
-      }).then(() => { if (this.userId) {
-      this.sectionService
-      .findSectionsForStudent(this.userId)
-      .then(sections => this.sections = sections ); }
       });
+      // .then(() => { if (this.userId) {
+      // this.sectionService
+      // .findSectionsForStudent(this.userId)
+      // .then(sections => this.sections = sections ); }
+      // });
+    //   .then(() => {
+    //     this.courseService.findAllCourses()
+    //       .then(courses => this.courses = courses);
+    //   })
+    //   .then(() => {
+    //     for (let i = 0; i < this.sections.length; i++) {
+    //       for (let j = 0; j < this.courses.length; j++) {
+    //         if (this.sections[i].section.courseId === this.courses[j].id) {
+    //           this.enrolledCourses.push(this.courses[j]);
+    //         }
+    //       }
+    //     }
+    //   }).then(() => {
+    //     console.log(this.sections);
+    //     console.log(this.courses);
+    //     console.log(this.userId);
+    // });
 
   }
 
